d7681a42f00ed23d495ffb77259c1bfad7b07f5f
yuhara <yuharacomplex@gmail.com>
2017-01-26 13:25:57 +0900

    dup3.goにコメント付与

---
+++ ch1/dup3/dup3.go
@@ -0,0 +1,43 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 12.
+
+//!+
+
+// Dup3 prints the count and text of lines that
+// appear more than once in the named input files.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		// ReadFile:指定したファイルを全読み込み
+		// stringであろうバイトのスライスを返却
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		fmt.Println("バイトのスライス")
+		fmt.Println(data)
+		// Split:指定した文字で分割し、スライスを返却
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+//!-
