0dccf2379e51e47ef2145e72f9c52120275bfc6b
yuhara <yuharacomplex@gmail.com>
2017-02-07 14:55:21 +0900

    fetchへコメント追加

---
+++ ch1/fetch/fetch.go
@@ -0,0 +1,38 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		// HTTPリクエスト発行
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		// resp.Body 読み込み可能なストリームとしてサーバからのレスポンスを含む
+		// ioutil.ReadAll レスポンス全体を読み込み
+		b, err := ioutil.ReadAll(resp.Body)
+		// 資源のリークを防ぐためにclose
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
+
+//!-
